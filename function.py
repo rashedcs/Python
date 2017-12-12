51207de15e6622c19cb76f8d6b6a553f737cc2ac
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 01:59:25 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,22 @@
+
+
+
+def sum( arg1, arg2 ):
+     total = arg1+arg2
+     return total
+
+
+n = int(input())
+m = int(input())
+
+total = sum(n,m)
+print (total)
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+n = int(input())
+    
+    
+for i in range(1, n+1):
+        b = i
+        print(b, end=" ")
+print
+
+       
