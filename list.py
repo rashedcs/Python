0d294ede62c63eb93b869ecbb936e431870a8d55
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 05:33:11 +0600

    Update list.py

--- list.py
+++ list.py
@@ -1,15 +1,8 @@
-n, k = map(int,input().split())
+n, k = map(int,input().split())   # jokhn aksathe 2 or more input nibo
 l = [int(n) for n in input().split()]
 
 print(l)
+print(k)
+print(n)
 
-i = 0
-while i <= n - 1:
-    if l[i] > 0:
-        if l[i] >= l[k - 1]:
-            i = i + 1
-        else :
-            break
-    else :
-        break
-print(int(i))
+
