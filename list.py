1e8604e82135abcc403ee70884781fc723812227
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 05:36:33 +0600

    list mane ak line

--- list.py
+++ list.py
@@ -1,4 +1,6 @@
-n, k = map(int,input().split())   # jokhn aksathe 2 or more input nibo
+#n, k = map(int,input().split())   # jokhn aksathe 2 or more input nibo
+n = int(input())
+k = int(input())
 l = [int(n) for n in input().split()]
 
 print(l)
