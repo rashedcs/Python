042a2de91c7497ab3ef8d10efdb56e174c7dbe90
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 05:22:18 +0600

    Create sum of list.py

---
+++ sum of list.py
@@ -0,0 +1,6 @@
+num = int(input())
+lst = []
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)            
+print("Sum of elements in given list is :", sum(lst))
