4f42fbd27c7b64079c24d6bf84c3011470e1d65b
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 22:05:51 +0600

    rectangle class

---
+++ class.py
@@ -0,0 +1,15 @@
+class rectangle : 
+    def __init__(self):
+       print("Area : ")
+    
+    def area(self, x, y):
+        return x*y
+
+
+
+obj = rectangle()
+length=5
+wide=9
+area = obj.area(length, wide)
+print (area)
+
