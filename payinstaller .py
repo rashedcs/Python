a9a25b182e35db3042ba42818e015fe3a4abc41e
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-13 20:15:02 +0600

    Create payinstaller .py

---
+++ payinstaller .py
@@ -0,0 +1,12 @@
+Using Payinstaller :
+
+1. At first install python pip
+sudo apt install python-pip  
+
+2. Then install
+
+sudo pip install PyInstaller
+
+3. Convert into exe
+
+pyinstaller --onefile <script_name>
