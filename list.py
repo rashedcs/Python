7a79fc44a498a3839c46133a6a4ae48a6c9caabb
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-25 14:45:31 +0600

    list

--- list.py
+++ list.py
@@ -1,16 +1,38 @@
 #n, k = map(int,input().split())   # jokhn aksathe 2 or more input nibo
 
-#Problem : Input a n number and n numbers of elements
+Problem : Input a n number and n numbers of elements
+    
+Case - 1 :
+    
 #input : 
     5
     1 2 3 4 5
-    
  #Output :
    [ 1 2 3 4 5]
   
-
 n = int(input())
 arr = list(map(int, input().split()))
 print(arr)
 
 
+Case 2 :
+    input : 
+      Number of element : 5
+        1
+        2
+        3
+        4
+        5
+        
+     Output : [ 1 2 3 4 5]
+        
+num = int(input())
+arr = list()
+
+print("Enter elements :")
+
+for i in range(num) :
+  ele  = int(input())
+  arr.append(ele)
+
+print(arr)
