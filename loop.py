60fc24f03c779494ce31aee54433d7e6cb79406e
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-25 21:40:02 +0600

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,9 +1,16 @@
-n = int(input())
-    
-    
+n = int(input())  
 for i in range(1, n+1):
         b = i
         print(b, end=" ")
 print
 
        
+n = int(input())
+arr = list(map(int, input().split()))
+for i in range(0, n):
+	print(arr[i], end=" ")
+    
+    
+    
+    
+    
