4005361591bd6f691b88ee4211fb0b59a65fc36e
Rashedul Islam <rashedcs@users.noreply.github.com>
2017-12-25 21:43:46 +0600

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,10 +1,22 @@
+
+#iterate 1 to n
 n = int(input())  
-for i in range(1, n+1):
+for i in range(1, n):
         b = i
         print(b, end=" ")
 print
 
        
+#iterate over element 
+n = int(input())
+arr = list(map(int, input().split()))
+for x in arr:
+	print(x, end=" ")
+	
+
+	
+	
+#iterate over index and element	
 n = int(input())
 arr = list(map(int, input().split()))
 for i in range(0, n):
